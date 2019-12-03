f000f5341dcb91f33884dd4d6db2860203a8712c
Marjolein Tromp <marjolein.tromp@incentro.com>
2019-12-03 14:57:00 +0100

    format if else prettier

--- week-1/if-else-basics.js
+++ week-1/if-else-basics.js
@@ -1,8 +1,8 @@
-// Verander deze input om te zien of je code werkt. 
+// Verander deze input om te zien of je code werkt.
 // Comment bijvoorbeeld regel 8 tot 11 weer aan, en regel 3 tot 5 uit.
 const age = 18;
 const isFemale = true;
-const driverStatus = 'bob';
+const driverStatus = "bob";
 
 /*
 Testdata
@@ -11,20 +11,22 @@ const isFemale = false;
 const driverStatus = 808; 
 */
 
-if(age >= 18){
-    console.log("Je mag naar binnen")
+if (age >= 18) {
+  console.log("Je mag naar binnen");
 } else {
-    console.log("Nope, je mag niet naar binnen")
+  console.log("Nope, je mag niet naar binnen");
 }
 
-if(isFemale){
-    console.log("Je ticket zegt dat je een vrouw bent!")
+if (isFemale) {
+  console.log("Je ticket zegt dat je een vrouw bent!");
 } else {
-    console.log("Helaas je hebt aangegeven op je ticket dat je een man bent, je mag niet naar binnen")
+  console.log(
+    "Helaas je hebt aangegeven op je ticket dat je een man bent, je mag niet naar binnen"
+  );
 }
 
-if(driverStatus === 'bob'){
-    console.log("Je bent de bob, je mag autorijden")
+if (driverStatus === "bob") {
+  console.log("Je bent de bob, je mag autorijden");
 } else {
-    console.log("Je bent geen bob, hier met die autosleutels")
+  console.log("Je bent geen bob, hier met die autosleutels");
 }
