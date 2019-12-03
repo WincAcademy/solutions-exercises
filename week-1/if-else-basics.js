c1b03c6bb9118a0e8cd9cd9c41f48b1bce9d0cb5
Marjolein Tromp <marjolein.tromp@incentro.com>
2019-12-03 14:41:28 +0100

    solution: if else statements

---
+++ week-1/if-else-basics.js
@@ -0,0 +1,30 @@
+// Verander deze input om te zien of je code werkt. 
+// Comment bijvoorbeeld regel 8 tot 11 weer aan, en regel 3 tot 5 uit.
+const age = 18;
+const isFemale = true;
+const driverStatus = 'bob';
+
+/*
+Testdata
+const age = 16; 
+const isFemale = false;
+const driverStatus = 808; 
+*/
+
+if(age >= 18){
+    console.log("Je mag naar binnen")
+} else {
+    console.log("Nope, je mag niet naar binnen")
+}
+
+if(isFemale){
+    console.log("Je ticket zegt dat je een vrouw bent!")
+} else {
+    console.log("Helaas je hebt aangegeven op je ticket dat je een man bent, je mag niet naar binnen")
+}
+
+if(driverStatus === 'bob'){
+    console.log("Je bent de bob, je mag autorijden")
+} else {
+    console.log("Je bent geen bob, hier met die autosleutels")
+}
