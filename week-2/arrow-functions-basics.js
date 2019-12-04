d659bc00775eecae46d8636cbe5c2897a3222c9c
Marjolein Tromp <marjolein.tromp@incentro.com>
2019-12-04 13:52:53 +0100

    arrow functions basics

---
+++ week-2/arrow-functions-basics.js
@@ -0,0 +1,23 @@
+// Opdracht A
+const ikRockArrowFunctions = () => {
+  console.log("Joe, ik rock de arrow functions!");
+};
+
+ikRockArrowFunctions();
+
+// Opdracht B
+const arrowFunction = () => console.log("Joe, ik rock de arrow functions!");
+
+arrowFunction();
+
+// Opdracht C
+() => 1 + 2;
+
+// Opdracht D
+const myFunction = (a, b) => a + b;
+
+// Opdracht E
+const addFive = a => a + 5;
+
+// Opdracht F
+const createObject = () => ({ value: "hoi" });
