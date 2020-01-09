eda547054511029087b271c3939e2f46741107f2
Ramon Sistermans <ramon.sistermans@incentro.com>
2020-01-09 23:31:56 +0100

    Processed PR feedback

--- week-4/password-verifier/password-verifier.test.js
+++ week-4/password-verifier/password-verifier.test.js
@@ -1,45 +1,57 @@
 const passwordVerifier = require('./password-verifier');
 
 describe("Password verifier", () => {
+    
     test("Password should not be blank", () => {
         const bad = passwordVerifier('');
         const better = passwordVerifier('x');
         expect(better.score).toBeGreaterThan(bad.score);
     });
+    
     test("Password should be at most 8 characters", () => {
         const bad = passwordVerifier('wincacademy');
         const better = passwordVerifier('winc');
         expect(better.score).toBeGreaterThan(bad.score);
     });
+    
     test("Password should have a lowercase character", () => {
         const bad = passwordVerifier('$^@#%');
         const better = passwordVerifier('winc');
         expect(better.score).toBeGreaterThan(bad.score);
     });
+    
     test("Password should have an uppercase character", () => {
         const bad = passwordVerifier('winc');
         const better = passwordVerifier('Winc');
         expect(better.score).toBeGreaterThan(bad.score);
     });
+    
     test("Password should have a digit character", () => {
         const bad = passwordVerifier('Winc');
         const better = passwordVerifier('Winc5');
         expect(better.score).toBeGreaterThan(bad.score);
     });
+    
     test("Password should always be false if it doesn't have a lowercase character", () => {
         const result = passwordVerifier('WINC5');
-        expect(result.valid).toBeFalsy();
+        expect(result.valid).toBe(false);
     });
+
     test("Password should only be valid if score is at least 3", () => {
+
         const test1 = passwordVerifier('');
+        expect(test1.valid).toBe(false);
+        
         const test2 = passwordVerifier('wincacademy');
+        expect(test2.valid).toBe(false);
+        
         const test3 = passwordVerifier('WINCACADEMY');
+        expect(test3.valid).toBe(false);
+        
         const test4 = passwordVerifier('winc5');
+        expect(test4.valid).toBe(true);
+        
         const test5 = passwordVerifier('WincAcademy5');
-        expect(test1.valid).toBeFalsy();
-        expect(test2.valid).toBeFalsy();
-        expect(test3.valid).toBeFalsy();
-        expect(test4.valid).toBeTruthy();
-        expect(test5.valid).toBeTruthy();
+        expect(test5.valid).toBe(false);
     });
 });
