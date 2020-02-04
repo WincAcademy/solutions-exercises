126073ae336676a919fb8018607cb85c95bde70e
Marjolein Tromp <marjolein.tromp@incentro.com>
2020-02-04 09:38:33 +0100

    gebruik je eerste javascript

---
+++ week-1/gebruik-eerste-javascript/script.js
@@ -0,0 +1,20 @@
+console.log("Hello Winc Academy");
+let name = "Marjolein";
+console.log(name);
+// You cannot make a variable with the name if
+let sum = 3 + 4;
+console.log("sum:", sum);
+let sumWithStrings = "3" + "4";
+console.log("sum with strings:", sumWithStrings);
+let subtraction = 10 - 3;
+console.log("subtraction:", subtraction);
+let multiplication = 1483 * 6342;
+console.log("multiplication:", multiplication);
+let division = 12 / 3;
+console.log("division:", division);
+let modulus = 12 % 9;
+console.log("modulus:", modulus);
+let age = 48;
+console.log(typeof age);
+let ageAsString = "48";
+console.log(typeof ageAsString);
