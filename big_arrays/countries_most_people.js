10032ee0f612a805b39df4cc4789e77f1f7bd1f7
Niels Bom <niels@nielsbom.com>
2020-05-23 13:34:45 +0200

    Add solution populous countries

---
+++ big_arrays/countries_most_people.js
@@ -0,0 +1,49 @@
+const getCountriesWithAmountOfPeople = personData => {
+  let countries = {};
+
+  // Count amount of people per country
+  // Unpack immediately
+  personData.forEach(({ region }) => {
+    if (region in countries) {
+      countries[region]++;
+    } else {
+      countries[region] = 1;
+    }
+  });
+
+  // So now we have an object with keys country and values amount of people.
+  // Convert into an array of objects so we can sort.
+  countries = Object.entries(countries);
+
+  // Convert into array of objects with country name and amount of people.
+  countries = countries.map(country => ({
+    country: country[0],
+    inhabitants: country[1],
+  }));
+
+  // Sort by most populous
+  countries.sort(
+    (country1, country2) => country1.inhabitants < country2.inhabitants
+  );
+  return countries;
+};
+
+// Unpack immediately
+const generateCountryAndInhabitantsHTML = ({ country, inhabitants }) => {
+  const li = document.createElement("li");
+  li.innerHTML = `${country} - ${inhabitants}`;
+  return li;
+};
+
+const displayCountriesSortedByPeople = () => {
+  const resultList = document.querySelector(".results");
+  resultList.innerHTML = "";
+  const countries = getCountriesWithAmountOfPeople(randomPersonData);
+  countries
+    .map(generateCountryAndInhabitantsHTML)
+    .forEach(li => resultList.appendChild(li));
+};
+
+document
+  .querySelector(".countries_most_people")
+  .addEventListener("click", displayCountriesSortedByPeople);
