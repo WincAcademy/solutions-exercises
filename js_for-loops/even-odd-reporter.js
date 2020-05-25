aced7948ad4da5059d9724e271827169d6d945cc
Marjolein Tromp <marjolein.tromp@incentro.com>
2020-05-25 12:48:33 +0200

    add for loops exercise

---
+++ js_for-loops/even-odd-reporter.js
@@ -0,0 +1,7 @@
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i + " is even");
+  } else {
+    console.log(i + " is oneven");
+  }
+}
---
+++ js_for-loops/multiplication-tables.js
@@ -0,0 +1,14 @@
+// Multiplication tables
+const multiplier = 9;
+for (let i = 0; i <= 10; i++) {
+  let result = multiplier * i;
+  console.log(multiplier + " * " + i + " = " + result);
+}
+
+// Bonus
+for (let multiplier = 0; multiplier <= 10; multiplier++) {
+  for (let i = 0; i <= 10; i++) {
+    let result = multiplier * i;
+    console.log(multiplier + " * " + i + " = " + result);
+  }
+}
