7979fe2ad372f1c8f6c7133242a0e2874887752d
Niels Bom <niels@nielsbom.com>
2020-07-01 09:17:57 +0200

    Add arrow functions to solution

---
+++ js_callbacks/callbacks_arrow.js
@@ -0,0 +1,40 @@
+// Zelfde code, maar dan met arrow functions
+
+// Stap 1
+const huiswerkMaken = (vak, callback) => {
+  console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+  callback();
+};
+
+// Stap 2
+const huiswerkMaken = (vak, callback) => {
+  console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+  callback();
+};
+
+const klaarMetHuiswerk = () =>
+  console.log("Kijk Paps/Mams, ik ben klaar met mijn huiswerk!");
+
+// Stap 3
+const huiswerkMaken = (vak, callback) => {
+  console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+  callback();
+};
+
+const klaarMetHuiswerk = () =>
+  console.log("Kijk Paps/Mams, ik ben klaar met mijn huiswerk!");
+
+huiswerkMaken("wiskunde", klaarMetHuiswerk);
+
+// Stap 4
+const huiswerkMaken = (vak, callback) => {
+  console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+  setTimeout(function () {
+    callback();
+  }, 10000);
+};
+
+const klaarMetHuiswerk = () =>
+  console.log("Kijk Paps/Mams, ik ben klaar met mijn huiswerk!");
+
+huiswerkMaken("wiskunde", klaarMetHuiswerk);
