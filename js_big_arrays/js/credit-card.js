28a6c073bd647cbc07d6ef36bdd18965718e6cc0
Niels Bom <niels@nielsbom.com>
2020-11-10 14:59:06 +0100

    Update big array solution

--- js_big_arrays/js/credit-card.js
+++ js_big_arrays/js/credit-card.js
@@ -1,66 +1,78 @@
 const isAdult = person => person.age > 17;
 
-const convertExpirationDate = credit_card => {
-  const expiration = credit_card.credit_card.expiration.split("/");
-  const expirationYear = parseInt("20" + expiration[1]);
-  const expirationMonth = parseInt(expiration[0]) - 1; // Jan = 0, Dec = 11
-  const expirationDay = 1; // Default
-  credit_card.expiration_date = new Date(
-    expirationYear,
-    expirationMonth,
-    expirationDay
-  );
-  return credit_card;
+const convertExpirationDate = person => {
+    // expiration: ["1", "21"]
+    const expiration = person.credit_card.expiration.split("/");
+    // parseInt("20" + "21")
+    const expirationYear = parseInt("20" + expiration[1]);
+    const expirationMonth = parseInt(expiration[0]) - 1; // Jan = 0, Dec = 11
+    const expirationDay = 1; // Default
+    person.credit_card.expiration_date = new Date(
+        expirationYear,
+        expirationMonth,
+        expirationDay
+    );
+    return person;
 };
 
 // Take out the expiration_date immediately.
-const cardExpiresInOneYear = ({ expiration_date }) => {
-  // Thanks Stack Overflow https://stackoverflow.com/a/29052008
-  const oneYearFromNow = new Date(
-    new Date().setFullYear(new Date().getFullYear() + 1)
-  );
-  const now = new Date();
-
-  return expiration_date > now && expiration_date < oneYearFromNow;
+const cardExpiresInOneYear = ({ credit_card }) => {
+    // Thanks Stack Overflow https://stackoverflow.com/a/29052008
+    const oneYearFromNow = new Date(
+        new Date().setFullYear(new Date().getFullYear() + 1)
+    );
+    const now = new Date();
+    return (
+        credit_card.expiration_date > now &&
+        credit_card.expiration_date < oneYearFromNow
+    );
 };
 
-const sortByExpirationDate = (card1, card2) =>
-  sort_helper(card1.expiration_date > card2.expiration_date);
+const sortByExpirationDate = (person1, person2) => {
+    return sort_helper(
+        person1.credit_card.expiration_date > person2.credit_card.expiration_date
+    );
+};
 
-const getCreditCardsThatWillExpire = () =>
-  randomPersonData
-    .filter(isAdult)
-    .map(convertExpirationDate)
-    .filter(cardExpiresInOneYear)
-    // Earlier dates at the top.
-    .sort(sortByExpirationDate);
+const getCreditCardsThatWillExpire = () => {
+    const adults = randomPersonData.filter(isAdult);
+    const adultsWithNiceExpirationDatesOnCCs = adults.map(convertExpirationDate);
+    const adultsWithExpiredCards = adultsWithNiceExpirationDatesOnCCs.filter(
+        cardExpiresInOneYear
+    );
+    // // Earlier dates at the top.
+    const sortedAdultsWithExpiredCards = adultsWithExpiredCards.sort(
+        sortByExpirationDate
+    );
+    return sortedAdultsWithExpiredCards;
+};
 
 const generateCCHTML = card => {
-  const li = document.createElement("li");
+    const li = document.createElement("li");
 
-  const name = document.createElement("span");
-  name.innerHTML = `${card.name} ${card.surname}`;
+    const name = document.createElement("span");
+    name.innerHTML = `${card.name} ${card.surname}`;
 
-  const phone = document.createElement("span");
-  phone.innerHTML = `Phone: ${card.phone}`;
+    const phone = document.createElement("span");
+    phone.innerHTML = `Phone: ${card.phone}`;
 
-  const card_details = document.createElement("span");
-  card_details.innerHTML = `Card: ${card.credit_card.number}`;
+    const card_details = document.createElement("span");
+    card_details.innerHTML = `Card: ${card.credit_card.number}`;
 
-  const expires = document.createElement("span");
-  expires.innerHTML = `Expires: ${card.credit_card.expiration}`;
+    const expires = document.createElement("span");
+    expires.innerHTML = `Expires: ${card.credit_card.expiration}`;
 
-  li.appendChild(name);
-  li.appendChild(phone);
-  li.appendChild(card_details);
-  li.appendChild(expires);
+    li.appendChild(name);
+    li.appendChild(phone);
+    li.appendChild(card_details);
+    li.appendChild(expires);
 
-  return li;
+    return li;
 };
 
 const displayOldCreditcardList = () =>
-  getCreditCardsThatWillExpire().map(generateCCHTML).forEach(addToResultList);
+    getCreditCardsThatWillExpire().map(generateCCHTML).forEach(addToResultList);
 
 document
-  .querySelector(".credit-card")
-  .addEventListener("click", displayOldCreditcardList);
+    .querySelector(".credit-card")
+    .addEventListener("click", displayOldCreditcardList);
