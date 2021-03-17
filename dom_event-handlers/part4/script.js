2ceb4084084e0ad67617c9db3d73c8cd0bb68741
Niels Bom <niels@nielsbom.com>
2021-03-17 14:44:08 +0100

    Update solution, thanks Asta!

--- dom_event-handlers/part4/script.js
+++ dom_event-handlers/part4/script.js
@@ -1,18 +1,20 @@
-const toggleClassBlueBackground = function () {
-  const bodyElement = document.body;
-  bodyElement.classList.toggle("red-background");
+const attachEventToAlertButton = function() {
+    const alertButton = document.getElementById("alert-button");
+    alertButton.addEventListener("click", function() {
+        alert("Hello World!");
+    });
 };
-const attachEventToChangeColorButton = function () {
-  const changeColorButton = document.getElementById("change-background-button");
-  changeColorButton.addEventListener("click", function () {
-    changeClassBlueBackground();
-  });
+
+const toggleColor = function() {
+    const bodyElement = document.body;
+    bodyElement.classList.toggle("red-background");
 };
-const attachEventToAlertButton = function () {
-  const alertButton = document.getElementById("alert-button");
-  alertButton.addEventListener("click", function () {
-    alert("Hello World!");
-  });
+
+const attachEventToChangeColorButton = function() {
+    const changeColorButton = document.getElementById("change-background-button");
+    changeColorButton.addEventListener("click", function() {
+        toggleColor();
+    });
 };
 
 attachEventToAlertButton();
