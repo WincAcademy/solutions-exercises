5e78febaf6d9ced0f578fa94ce404308b4cfcc30
Niels Bom <niels@nielsbom.com>
2021-03-25 11:51:06 +0100

    Remove unused code

--- gatsby/days_of_the_week/src/pages/index.js
+++ gatsby/days_of_the_week/src/pages/index.js
@@ -1,14 +1,5 @@
 import React from "react";
-import { Link } from "gatsby";
 import Layout from "../components/Layout";
-
-const generateDayLink = ({ number, name }) => (
-  <li key={number} className="day_link">
-    <Link className={`day${number}`} to={`/${number}`}>
-      {name}
-    </Link>
-  </li>
-);
 
 export default function Home() {
   return (
