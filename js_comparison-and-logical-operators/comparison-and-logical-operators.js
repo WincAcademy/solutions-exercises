68ec97505367464c17a1cdd0ebe7247413f0bcd9
Mark van der Laan <mrkvdl@gmail.com>
2021-12-31 09:59:00 +0100

    translate comparison-and-logical-operators

--- js_comparison-and-logical-operators/comparison-and-logical-operators.js
+++ js_comparison-and-logical-operators/comparison-and-logical-operators.js
@@ -1,5 +1,5 @@
-// Verander deze input om te zien of je code werkt.
-// Comment bijvoorbeeld regel 10 tot 15 weer aan, en regel 3 tot 8 uit.
+// Change these inputs to see if your code works.
+// For example, comment lines 10 to 15 back on, and lines 3 to 8 off.
 const age = 18;
 const isFemale = true;
 const driverStatus = "bob";
@@ -7,7 +7,7 @@ const name = "Sarah";
 const totalAmount = 27;
 
 /*
-Testdata
+Test dates
 const age = 16; 
 const isFemale = false;
 const driverStatus = 'b0b'; 
@@ -16,41 +16,41 @@ const totalAmount = 5;
 */
 
 if (age >= 18) {
-  console.log("Je bent 18 jaar of ouder, je mag naar binnen");
+  console.log("You are 18 or older, you may enter");
 } else {
-  console.log("Nope, je mag niet naar binnen");
+  console.log("Nope, you are not allowed to enter");
 }
 
 if (age >= 18 && age <= 25) {
-  console.log("Je krijgt 50% korting op je biertje");
+  console.log("You get 50% off your beer");
 }
 
 if (isFemale) {
-  console.log("Je ticket zegt dat je een vrouw bent!");
+  console.log("Your ticket says you're a female!");
 } else {
   console.log(
-    "Helaas je hebt aangegeven op je ticket dat je een man bent, je mag niet naar binnen"
+    "Unfortunately you indicated on your ticket that you are a male, you are not allowed to enter".
   );
 }
 
 if (driverStatus === "bob") {
-  console.log("Je bent de bob, je mag autorijden");
+  console.log("You are the bob, you are allowed to drive");
 } else {
-  console.log("Je bent geen bob, hier met die autosleutels");
+  console.log("You're not a bob, hand in those car keys!");
 }
 
 if (name === "Sarah" || name === "Bram") {
-  console.log("Je naam is Sarah of Bram, je krijgt een gratis biertje!");
+  console.log("Your name is Sarah or Bram, you get a free beer!");
 } else {
-  console.log("Je doet niet mee aan onze ludieke Sarah Abraham actie");
+  console.log("You are not participating in our fun Sarah Abraham promotion");
 }
 
 if (totalAmount >= 100) {
-  console.log("gratis flesje champagne.");
+  console.log("free bottle of champagne.");
 } else if (totalAmount > 50) {
-  console.log("gratis portie nachos");
+  console.log("free serving of nachos");
 } else if (totalAmount > 25) {
-  console.log("gratis (vega)bitterballen");
+  console.log("free (vega) bitterballs");
 } else {
-  console.log("Helaas geen korting, maar leuk dat je er bent");
+  console.log("Unfortunately you don't get a discount, but it's nice to have you!");
 }
