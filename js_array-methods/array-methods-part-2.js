a0e67a116b66180adf3fc8c97cb5a022d573294b
Thomas Sterrenburg <th.sterrenburg@gmail.com>
2022-04-20 14:06:10 +0200

    fix: correct expected output, thank you Vera

--- js_array-methods/array-methods-part-2.js
+++ js_array-methods/array-methods-part-2.js
@@ -2,19 +2,19 @@
 const superheroes = [
   { name: "Batman", alter_ego: "Bruce Wayne" },
   { name: "Superman", alter_ego: "Clark Kent" },
-  { name: "Spiderman", alter_ego: "Peter Parker" }
+  { name: "Spiderman", alter_ego: "Peter Parker" },
 ];
-const findSpiderMan = function(superheroes) {
-  return superheroes.find(function(superhero) {
+const findSpiderMan = function (superheroes) {
+  return superheroes.find(function (superhero) {
     return superhero.name === "Spiderman";
   });
 };
 console.log("Find Spiderman:", findSpiderMan(superheroes));
 
 // B Double Array values
-const doubleArrayValues = function(array) {
+const doubleArrayValues = function (array) {
   let newArray = [];
-  array.forEach(number => {
+  array.forEach((number) => {
     newArray.push(number * 2);
   });
   return newArray;
@@ -23,14 +23,14 @@ const doubleArrayValues = function(array) {
 // de onderstaande oplossingen zijn voor de pro's en gebruiken de .map functie*
 // *die je aan het eind van vandaag ook gaat zijn.
 
-const doubleArrayValuesArrow = function(array) {
-  return array.map(number => {
+const doubleArrayValuesArrow = function (array) {
+  return array.map((number) => {
     return number + number;
   });
 };
 
-const doubleArrayValuesShortHand = function(x) {
-  return x.map(n => n + n);
+const doubleArrayValuesShortHand = function (x) {
+  return x.map((n) => n + n);
 };
 
 console.log("Double 1,2,3:", doubleArrayValues([1, 2, 3]));
@@ -38,11 +38,11 @@ console.log("Double 1,2,3:", doubleArrayValues([1, 2, 3]));
 console.log(doubleArrayValuesArrow([4, 1, 1, 1, 4]));
 // expected output: [8, 2, 2, 2, 8]);
 console.log(doubleArrayValuesShortHand([4, 1, 1, 1, 4]));
-// expected output: [4, 4, 4, 4, 4, 4]
+// expected output: [8, 2, 2, 2, 8]);
 
 // C Is Bigger than 10
-const containsNumberBiggerThan10 = function(array) {
-  return array.some(number => {
+const containsNumberBiggerThan10 = function (array) {
+  return array.some((number) => {
     return number > 10;
   });
 };
@@ -66,21 +66,21 @@ console.log(
     "Italy",
     "Japan",
     "United Kingdom",
-    "United States"
+    "United States",
   ])
 );
 
 // E
-const tenfold = function(array) {
+const tenfold = function (array) {
   let newArray = [];
-  array.forEach(number => {
+  array.forEach((number) => {
     newArray.push(number * 10);
   });
   return newArray;
 };
 // Of met een .map method
-const tenfoldMap = function(array) {
-  return array.map(number => {
+const tenfoldMap = function (array) {
+  return array.map((number) => {
     return number * 10;
   });
 };
@@ -92,7 +92,7 @@ console.log("tenfold", tenfoldMap([1, 4, 3, 6, 9, 7, 11]));
 // F
 
 function isBelow100(array) {
-  return array.every(number => {
+  return array.every((number) => {
     return number < 100;
   });
 }
@@ -108,7 +108,7 @@ console.log(
 // G
 const numbers = [1, 81, 4, 53, 3, 6, 11, 77, 84, 98, 101, 206, 234];
 
-const bigSum = function(array) {
+const bigSum = function (array) {
   return array.reduce((acc, number) => {
     return acc + number;
   });
