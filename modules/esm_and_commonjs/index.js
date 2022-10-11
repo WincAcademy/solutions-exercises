fe462ecc465237b0532ddfd9341410dc06900a3c
Niels Bom <niels@nielsbom.com>
2022-10-11 16:31:28 +0200

    Add ESM and CommonJS example

---
+++ modules/esm_and_commonjs/index.js
@@ -0,0 +1,20 @@
+import {
+    add,
+    subtract,
+    multiply,
+    divide,
+    speedOfLightMetersPerSecond,
+    square,
+} from "./lib.cjs";
+
+console.log(add(3, 4));
+
+console.log(subtract(5, 2));
+
+console.log(multiply(10, 4));
+
+console.log(divide(10, 4));
+
+console.log(square(9));
+
+console.log(`The speed of light: ${speedOfLightMetersPerSecond} m/s.`);
---
+++ modules/esm_and_commonjs/lib.cjs
@@ -0,0 +1,20 @@
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const square = a => a * a;
+
+const speedOfLightMetersPerSecond = 299792458;
+
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide,
+    square,
+    speedOfLightMetersPerSecond,
+};
